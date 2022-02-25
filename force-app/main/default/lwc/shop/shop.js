9c9d94af2893fdfb261b35e9dac9aa10566be6e0
Mountain Scott <mountainscott@yahoo.com>
2022-02-24 16:15:06 -0800

    replaced img src values with corresponding variables from shop.js

--- force-app/main/default/lwc/shop/shop.js
+++ force-app/main/default/lwc/shop/shop.js
@@ -8,8 +8,10 @@ export default class Shop extends LightningElement {
     s22_front = phone_accessory_images + '/Galaxy_s22_FRONT.png';
     iphone_12_back = phone_accessory_images + '/i12_BACK.png';
     iphone_12_front = phone_accessory_images + '/i12_FRONT.png';
-    iphone_pro_13_back = phone_accessory_images + '/i13_pro_BACK.png';
+    i13_mini_back = phone_accessory_images + '/iphone_13_mini_back.png';
+    i13_mini_front = phone_accessory_images + '/iphone_13_mini_front.png';
     iphone_pro_13_front = phone_accessory_images + '/i13_pro_FRONT.png';
+    iphone_pro_13_back = phone_accessory_images + '/i13_pro_BACK.png';
     pixel_6_back = phone_accessory_images + '/Pixel_6_BACK.png';
     pixel_6_front = phone_accessory_images + '/Pixel6_FRONT.png';
     rating_star_blank = phone_accessory_images + '/rating-star-blank.png';
@@ -19,4 +21,7 @@ export default class Shop extends LightningElement {
     spark_hotspot = phone_accessory_images + '/Spark_Hotspot.png';
     zFlip_back = phone_accessory_images + '/Z_flip3_BACK.png';
     zFlip_front = phone_accessory_images + '/Z_Flip3_FRONT.png';
+    i13_pro_MAX_BACK = phone_accessory_images + '/i13_pro_max_BACK.png';
+    i13_pro_MAX_FRONT = phone_accessory_images + '/i13_ProMax_FRONT.png';
+    netgear_nighthawk = phone_accessory_images + '/netgear-nighthalk.png';
 }
